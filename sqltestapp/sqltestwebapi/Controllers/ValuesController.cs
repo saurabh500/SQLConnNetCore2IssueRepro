d08de4304fee7e38380244a5157cc3d361f106d1
Saurabh Singh <sausing@microsoft.com>
2017-12-20 10:55:18 -0800

    Modify Values Controller to have all SqlAPIs

--- sqltestapp/sqltestwebapi/Controllers/ValuesController.cs
+++ sqltestapp/sqltestwebapi/Controllers/ValuesController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 
 using System.Net;
+using System.Diagnostics;
 
 namespace sqltestwebapi.Controllers
 {
@@ -17,7 +18,7 @@ namespace sqltestwebapi.Controllers
         [HttpGet("dapper")]
         public async Task<IActionResult> GetDapper()
         {
-            var connectionString = "Server=tcp:eshopsql-e3h5ug5xd3rfs.database.windows.net,1433;Initial Catalog=orderingdb;Persist Security Info=False;User ID=eshop;Password=Pass@word!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Max Pool Size=400;Connection Timeout=30;";
+            var connectionString = "Server=tcp:ss-desktop2.redmond.corp.microsoft.com,1433;Initial Catalog=fortune;Persist Security Info=False;User ID=saurabh;Password=HappyPass321;MultipleActiveResultSets=False;Max Pool Size=400;Connection Timeout=30;";
 
             using (var connection = new SqlConnection(connectionString))
             {
@@ -25,12 +26,7 @@ namespace sqltestwebapi.Controllers
                 {
                     connection.Open();
 
-                    var orders = await connection.QueryAsync<OrderSummary>(@"SELECT o.[Id] as ordernumber,o.[OrderDate] as [date],os.[Name] as [status],SUM(oi.units*oi.unitprice) as total
-                     FROM [ordering].[Orders] o
-                     LEFT JOIN[ordering].[orderitems] oi ON  o.Id = oi.orderid 
-                     LEFT JOIN[ordering].[orderstatus] os on o.OrderStatusId = os.Id                     
-                     GROUP BY o.[Id], o.[OrderDate], os.[Name] 
-                     ORDER BY o.[Id]");
+                    var orders = await connection.QueryAsync<Fortune>(@"SELECT * from FORTUNE");
 
                     return Ok(orders.Count());
 
@@ -42,10 +38,11 @@ namespace sqltestwebapi.Controllers
             }
 
         }
+
         [HttpGet("native")]
         public async Task<IActionResult> GetNative()
         {
-            var connectionString = "Server=tcp:eshopsql-e3h5ug5xd3rfs.database.windows.net,1433;Initial Catalog=orderingdb;Persist Security Info=False;User ID=eshop;Password=Pass@word!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Max Pool Size=400;Connection Timeout=30;";
+            var connectionString = "Server=tcp:sqlloadtest.database.windows.net,1433;Initial Catalog=fortune;Persist Security Info=False;User ID=saurabh;Password=HappyPass321;MultipleActiveResultSets=False;Max Pool Size=400;Connection Timeout=30;";
 
             using (var connection = new SqlConnection(connectionString))
             {
@@ -54,12 +51,7 @@ namespace sqltestwebapi.Controllers
                     connection.Open();
 
                     var command = connection.CreateCommand();
-                    command.CommandText = @"SELECT o.[Id] as ordernumber,o.[OrderDate] as [date],os.[Name] as [status],SUM(oi.units*oi.unitprice) as total
-                     FROM [ordering].[Orders] o
-                     LEFT JOIN[ordering].[orderitems] oi ON  o.Id = oi.orderid 
-                     LEFT JOIN[ordering].[orderstatus] os on o.OrderStatusId = os.Id                     
-                     GROUP BY o.[Id], o.[OrderDate], os.[Name] 
-                     ORDER BY o.[Id]";
+                    command.CommandText = @"SELECT * from FORTUNE";
 
                     var reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
                     var count = 0;
@@ -77,28 +69,71 @@ namespace sqltestwebapi.Controllers
                 }
             }
 
-            
+
         }
-        
+
+        [HttpGet("sqlasync")]
+        public async Task<IActionResult> GetSqlAsync()
+        {
+            var connectionString = "Server=tcp:sqlloadtest.database.windows.net,1433;Initial Catalog=fortune;Persist Security Info=False;User ID=saurabh;Password=HappyPass321;MultipleActiveResultSets=False;Max Pool Size=400;Connection Timeout=30;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"SELECT * from FORTUNE";
+
+                    using (var reader = await command.ExecuteReaderAsync(System.Data.CommandBehavior.CloseConnection))
+                    {
+                        var count = 0;
+                        while (await reader.ReadAsync())
+                        {
+                            count += 1;
+                        }
+                        return Ok(count);
+                    }
+                    
+
+                }
+                catch (Exception x)
+                {
+                    throw new InvalidOperationException(x.ToString());
+                }
+            }
+
+
+        }
+
         [HttpGet("sqlnet")]
         public async Task<IActionResult> GetNetwork()
         {
             string serverName = "eshopsql-e3h5ug5xd3rfs.database.windows.net";
             int port = 1433;
+            Stopwatch st = new Stopwatch();
+            st.Start();
             Task<Socket> connectTask = ConnectAsync(serverName, port);
-            connectTask.Wait(30 * 1000);
-            Socket _socket = connectTask.Result;
-            if(_socket !=null && _socket.Connected)
+            Socket _socket = await connectTask;
+            st.Stop();
+
+            if (_socket != null && _socket.Connected)
             {
-                Console.WriteLine("Socket Connected");
+                Console.WriteLine("Socket Connected " + st.ElapsedMilliseconds);
             }
-            
+            _socket.Dispose();
             return Ok(0);
         }
 
         private async Task<Socket> ConnectAsync(string serverName, int port)
         {
-            IPAddress[] addresses = await Dns.GetHostAddressesAsync(serverName).ConfigureAwait(false);
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            IPAddress[] addresses = Dns.GetHostAddresses(serverName);
+            //IPAddress[] addresses = await Dns.GetHostAddressesAsync(serverName).ConfigureAwait(false);
+            st.Stop();
+            Console.WriteLine("DNS Resolution Time " + st.ElapsedMilliseconds);
             IPAddress targetAddrV4 = Array.Find(addresses, addr => (addr.AddressFamily == AddressFamily.InterNetwork));
             {
                 IPAddress targetAddr = targetAddrV4;
@@ -106,7 +141,8 @@ namespace sqltestwebapi.Controllers
 
                 try
                 {
-                    await socket.ConnectAsync(targetAddr, port).ConfigureAwait(false);
+                    socket.Connect(targetAddr, port);
+                    //await socket.ConnectAsync(targetAddr, port).ConfigureAwait(false);
                 }
                 catch
                 {
@@ -118,11 +154,9 @@ namespace sqltestwebapi.Controllers
         }
     }
 
-    public class OrderSummary
+    public class Fortune
     {
-        public int ordernumber { get; set; }
-        public DateTime date { get; set; }
-        public string status { get; set; }
-        public double total { get; set; }
+        public int Id { get; set; }
+        public string Message { get; set; }
     }
 }
